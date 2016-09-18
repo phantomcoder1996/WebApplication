73d85571e6938404167619e9ffc8b34b12f1b3c1
Reem <macbookpro@MacBooks-MBP.Home>
2016-09-18 23:12:03 +0200

    TEST 2: EXPERIENCE TEMPLATE USAGE modified branch template and experienced using the render_template function

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from app import app
+from flask import render_template
 @app.route('/')
 @app.route('/index')
-def Hello():
- 	return "Hello World :D"
+def index():
+ 	user={'nickname':'Jones'}
+ 	posts=[{'author':{'nickname':'Jones'},'body':'Beautiful day in Portland !'},
+{'author':{'nickname':'Reem'},'body':'Hi everyone !'}
+ 	]
+ 	return render_template("index.html",user=user,posts=posts)
